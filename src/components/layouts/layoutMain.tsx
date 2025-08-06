d41ef38ecab7b0cd86591469b63fa8399615d34a
Tadd <2251120340@ut.edu.vn>
2025-08-06 16:13:58 +0700

    update lan 2

--- src/components/layouts/layoutMain.tsx
+++ src/components/layouts/layoutMain.tsx
@@ -2,19 +2,19 @@ import React from "react";
 import { Box } from "@mui/material";
 import { Outlet } from "react-router-dom";
 
-import Footer from "./layoutFooter";
-import Header from "./layoutHeader";
+// import Footer from "./layoutFooter";
+// import Header from "./layoutHeader";
 
 const MainLayout = () => {
   return (
     <Box sx={{ display: "flex", flexDirection: "column", minHeight: "100vh" }}>
-      <Header />
+      {/* <Header /> */}
 
       <Box component="main" sx={{ flex: 1 }}>
         <Outlet />
       </Box>
 
-      <Footer />
+      {/* <Footer /> */}
     </Box>
   );
 };
